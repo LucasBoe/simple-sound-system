dea03407242f27e59b3868639f9a818f0c54d684
LucasBoe <lucas@lumotex.de>
2022-09-21 16:53:37 +0200

    fixed error

--- Runtime/Sound.cs
+++ Runtime/Sound.cs
@@ -30,7 +30,7 @@ namespace SoundSystem
         public PlayingSound Play() { return SoundManager.Instance.Play(this); }
         public PlayingSound PlayLoop(float fadeDuration = 0.2f) { return SoundManager.Instance.Play(this, loop: true, fadeDuration); }
 
-        public void Play() => SoundManager.Instance.Play(this);
+        public void PlaySound() => SoundManager.Instance.Play(this);
         public void Stop() => SoundManager.Instance.Stop(this);
         public void StopLoop() => Stop();
 
